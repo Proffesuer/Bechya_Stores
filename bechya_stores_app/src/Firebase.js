9a23b3f1156a2cd970dcf4b7f904ba4da226471a
benmakokha <80826990+benmakokha@users.noreply.github.com>
2022-11-13 21:23:12 +0300

    comment

---
+++ bechya_stores_app/src/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgqD1aUOse8isxlq4LU0kCzfu6DOO5sVU",
+  authDomain: "bechyastock.firebaseapp.com",
+  databaseURL: "https://bechyastock-default-rtdb.firebaseio.com",
+  projectId: "bechyastock",
+  storageBucket: "bechyastock.appspot.com",
+  messagingSenderId: "533552825039",
+  appId: "1:533552825039:web:77b4c5832d413145faadba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
